fc3f4df20221308bcc7eccb51b72dab10425bf31
Zach Arguelles <admin@arguelles.me>
2026-01-20 13:51:52 -0500

    feat(01-02): create Lure Mark Tiptap extension
    - Installed uuid and @types/uuid for unique ID generation
    - Created LureMark Node extension wrapping text in span[data-lure-id]
    - Extension adds 'lure-mark' class for CSS styling
    - Parses existing span[data-lure-id] elements from HTML
    - Generates unique UUID for each Lure Mark instance

---
+++ src/extensions/LureMark.ts
@@ -0,0 +1,39 @@
+import { Node, mergeAttributes } from '@tiptap/core'
+import { v4 as uuidv4 } from 'uuid'
+
+export const LureMark = Node.create({
+  name: 'lureMark',
+
+  group: 'inline',
+
+  inline: true,
+
+  atom: true,
+
+  addAttributes() {
+    return {
+      lureId: {
+        default: uuidv4(),
+      },
+    }
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: 'span[data-lure-id]',
+      },
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return [
+      'span',
+      mergeAttributes(HTMLAttributes, {
+        'data-lure-id': HTMLAttributes.lureId,
+        class: 'lure-mark',
+      }),
+      0,
+    ]
+  },
+})
